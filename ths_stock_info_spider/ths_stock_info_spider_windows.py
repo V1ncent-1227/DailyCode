300b07c3a05ffd08305b48a1decb50a6c4d349d3
xinhaiyucheng <1726401743@qq.com>
2020-08-19 18:24:27 +0800

    添加搜索和爬取港股、美股功能

--- ths_stock_info_spider/ths_stock_info_spider_windows.py
+++ ths_stock_info_spider/ths_stock_info_spider_windows.py
@@ -32,11 +32,41 @@ browser.get('http://www.iwencai.com/stockpick/search?tid=stockpick&qs=box_main_t
 #print(browser.page_source)
 trs = browser.find_elements_by_xpath('//*[@id="tableWrap"]/div[2]/div/div[2]/div/table/tbody/tr')
 result = ''
+result += 'A股' + '\n'
+print('A股')
+for tr in trs:
+    code = tr.find_element_by_xpath('td[3]/div').text
+    name = tr.find_element_by_xpath('td[4]/div/a').text
+    ## 688-科创板，200-深圳B股，900-上证B股
+    if not code.startswith('688') and not code.startswith('200') and not code.startswith('900'):
+        print(code, name)
+        result += '* ' + code + ' ' + name + '\n'
+        logwrite(code + ' ' + name)
+#print(result)
+
+# 港股
+result += '\n' + '港股' + '\n'
+print('港股')
+browser.get('http://www.iwencai.com/stockpick/search?typed=1&preParams=&ts=1&f=1&qs=result_rewrite&selfsectsn=&querytype=hkstock&searchfilter=&tid=stockpick&w=cci%E5%B0%8F%E4%BA%8E-100%E5%92%8Cj%E5%B0%8F%E4%BA%8E0%E5%92%8Ck%E5%B0%8F%E4%BA%8E30%E5%92%8Cd%E5%B0%8F%E4%BA%8E30%E5%92%8Cmacd%E5%B0%8F%E4%BA%8E0')
+trs = browser.find_elements_by_xpath('//*[@id="tableWrap"]/div[2]/div/div[2]/div/table/tbody/tr')
 for tr in trs:
     code = tr.find_element_by_xpath('td[3]/div').text
     name = tr.find_element_by_xpath('td[4]/div/a').text
     print(code, name)
     result += '* ' + code + ' ' + name + '\n'
     logwrite(code + ' ' + name)
-#print(result)
+
+# 美股
+result += '\n' + '美股' + '\n'
+print('美股')
+browser.get('http://www.iwencai.com/stockpick/search?typed=1&preParams=&ts=1&f=1&qs=result_rewrite&selfsectsn=&querytype=usstock&searchfilter=&tid=stockpick&w=%E4%B8%AD%E6%A6%82%E8%82%A1cci%E5%B0%8F%E4%BA%8E-100%E5%92%8Cj%E5%B0%8F%E4%BA%8E0%E5%92%8Ck%E5%B0%8F%E4%BA%8E30%E5%92%8Cd%E5%B0%8F%E4%BA%8E30%E5%92%8Cmacd%E5%B0%8F%E4%BA%8E0')
+trs = browser.find_elements_by_xpath('//*[@id="tableWrap"]/div[2]/div/div[2]/div/table/tbody/tr')
+for tr in trs:
+    code = tr.find_element_by_xpath('td[3]/div').text
+    name = tr.find_element_by_xpath('td[4]/div/a').text
+    print(code, name)
+    result += '* ' + code + ' ' + name + '\n'
+    logwrite(code + ' ' + name)
+
+print(result)
 webhook(result)
